c86243ee1311848d5d737c135738cc51ca8c8982
yahir91 <yahir.cardona@gmail.com>
2024-02-28 16:11:47 -0600

    fix: contact url

--- src/sections/Contact/ContactInfo.ts
+++ src/sections/Contact/ContactInfo.ts
@@ -1,11 +1,11 @@
 export const CONTACT: Contact[] = [
-  { id: 1, logo: "/icons/email.png", name: "yahir.cardona@gmail.com" },
+  { id: 1, logo: "icons/email.png", name: "yahir.cardona@gmail.com" },
   {
     id: 2,
-    logo: "/icons/linkedin.png",
+    logo: "icons/linkedin.png",
     name: "www.linkedin.com/in/yahir-cardona",
   },
-  { id: 3, logo: "/icons/github.png", name: "github.com/yahir91" },
+  { id: 3, logo: "icons/github.png", name: "github.com/yahir91" },
 ];
 
 export interface Contact {
