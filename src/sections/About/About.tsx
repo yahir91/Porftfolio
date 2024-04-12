ed408b2c6b9f4621464ddb03fe219fe9d09f440d
yahir91 <yahir.cardona@gmail.com>
2024-04-12 11:53:24 -0600

    added skills

--- src/sections/About/About.tsx
+++ src/sections/About/About.tsx
@@ -7,13 +7,20 @@ const About = () => {
       <SectionDivider title="ABOUT" />
       <div className={classes.content}>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          My passion is building excellent software that improves the lives of
+          those around me.
+        </p>
+        <br />
+        <p>
+          I’ve always sought out opportunities and challenges that are
+          meaningful to me. Although my professional path has taken many twists
+          and turns - from having success in several civil engineer projects to
+          leading a hardware store company. I've never stopped engaging my
+          passion to help others and solve problems. As a web developer, I enjoy
+          using my obsessive attention to detail, my unequivocal love for making
+          things, and my mission-driven work ethic to literally change the
+          world. That's why I’m excited to make a big impact at a high growth
+          company.
         </p>
       </div>
     </div>
--- src/sections/Skills/SkillsInfo.ts
+++ src/sections/Skills/SkillsInfo.ts
@@ -1,23 +1,46 @@
 export const SKILLS: Skills[] = [
   {
     id: 1,
-    title: "Front End",
+    title: "Languages",
     skills: [
-      { logo: "skillsLogos/React.png", name: "React" },
-      { logo: "skillsLogos/React.png", name: "React" },
-      { logo: "skillsLogos/React.png", name: "React" },
-      { logo: "skillsLogos/React.png", name: "React" },
+      { logo: "skillsLogos/Javascript.png", name: "Javascript" },
+      { logo: "skillsLogos/Typescript.png", name: "Typescript" },
+      { logo: "skillsLogos/Ruby.png", name: "Ruby" },
+      { logo: "skillsLogos/Html.png", name: "HTML" },
+      { logo: "skillsLogos/Css.png", name: "CSS" },
     ],
   },
   {
     id: 2,
     title: "Front End",
-    skills: [{ logo: "skillsLogos/React.png", name: "React" }],
+    skills: [
+      { logo: "skillsLogos/React.png", name: "React" },
+      { logo: "skillsLogos/Next.svg", name: "Next.Js" },
+      { logo: "skillsLogos/Responsive.png", name: "Responsive Design" },
+      { logo: "skillsLogos/Scss.png", name: "Scss" },
+      { logo: "skillsLogos/Vite.png", name: "Vite" },
+    ],
   },
   {
     id: 3,
-    title: "Front End",
-    skills: [{ logo: "skillsLogos/React.png", name: "React" }],
+    title: "Back End",
+    skills: [
+      { logo: "skillsLogos/Nest.png", name: "Nest.Js" },
+      { logo: "skillsLogos/Node.svg", name: "Node.Js" },
+      { logo: "skillsLogos/Rails.svg", name: "Ruby on Rails" },
+      { logo: "skillsLogos/Postgresql.svg", name: "PostgreSQL" },
+      { logo: "skillsLogos/Prisma.svg", name: "Prisma" },
+      { logo: "skillsLogos/Amazon.svg", name: "Amazon" },
+    ],
+  },
+  {
+    id: 4,
+    title: "Testing",
+    skills: [
+      { logo: "skillsLogos/Jest.svg", name: "Jest" },
+      { logo: "skillsLogos/Rspec.ai", name: "Rspec" },
+      { logo: "skillsLogos/Vitest.svg", name: "Vitest" },
+    ],
   },
 ];
 
